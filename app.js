4b85c383756ab8aafaf00d1320e983688b82739f
Rune Vestvik <rvestvik@gmail.com>
2013-07-12 21:01:31 +0200

    First version up and running

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , util = require('util')
+  , fs = require('fs')
+  , execFile = require('child_process').execFile;    
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 4444);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -30,6 +31,49 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+app.get('/diff/:id', function(req, res) {
+  if (fs.existsSync(__dirname + "/public/images/"+req.params.id+"_diff")) {
+    res.send("<html><body><img src='/images/"+req.params.id+"'/>&nbsp;<img src='/images/"+req.params.id+"_diff'/>&nbsp;<img src='/images/"+req.params.id+".gif'/></body><br/><a href='/diff/"+req.params.id+"/accept'>Accept changes</a></html>");
+  }
+  else 
+    res.send(404);
+});
+
+app.get('/diff/:id/accept', function(req, res) {  
+  if (fs.existsSync(__dirname + "/public/images/"+req.params.id+"_diff")) {
+	fs.unlinkSync(__dirname + "/public/images/"+req.params.id);
+	fs.renameSync(__dirname + "/public/images/"+req.params.id+"_diff", __dirname + "/public/images/"+req.params.id);
+	res.send("Accepted!");
+  }
+  else
+    res.send(404);
+});
+
+app.post('/diff/:id', function(req, res) {
+  fs.readFile(req.files.image.path, function (err, data) {	  	  
+    var idPath = __dirname + "/public/images/"+req.params.id;
+    if (!fs.existsSync(idPath)) {
+      fs.writeFile(idPath, data, function (err) {
+        res.json({ status: 'new' })
+      });
+    }
+	else {
+	  fs.writeFile(idPath+"_diff", data, function (err) {
+	    execFile('D:\\Program Files (x86)\\ImageMagick-6.8.6-Q16\\compare.exe', ['-metric','AE', idPath, idPath+"_diff", 'diff.png'], function(err,tmp,difference){
+		  if (difference == 0)
+		    res.json({ status: 'same' })
+		  else
+		  {
+			execFile('D:\\Program Files (x86)\\ImageMagick-6.8.6-Q16\\convert.exe', ['-delay','25', idPath, idPath+"_diff", '-loop', '0', __dirname + "/public/images/"+req.params.id+".gif"], function(err){		  
+			  res.json({ status: 'different', difference:difference })
+			});
+		  }
+		});        
+      });
+	}
+  });
+});
+ 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
